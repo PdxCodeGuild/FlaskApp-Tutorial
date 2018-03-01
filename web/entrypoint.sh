dcaef5f449039a035801e789b0dd0458d3107027
kwmccabe <kwmccabe@yahoo.com>
2018-03-01 05:52:21 -0800

    Hello PythonDebugger

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -6,8 +6,8 @@
     flaskapp:app \
     --bind :8000 \
     --workers 2 \
-    --timeout 3600 \
     --access-logfile logs/gunicorn-access.log \
     --error-logfile logs/gunicorn-error.log \
     --log-level info \
+    --timeout 3600 \
     --reload
