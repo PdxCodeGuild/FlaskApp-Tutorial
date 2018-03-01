f100ab7cf4da8b82160bc1a39f8c109ebc485705
kwmccabe <kwmccabe@yahoo.com>
2018-03-01 11:42:01 -0800

    AppConfig: DevelopmentConfig, TestingConfig, ProductionConfig, ENV FLASK_CONFIG

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from config import AppConfig
+from config import config
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
 
-    app.config.from_object(AppConfig)
-    AppConfig.init_app(app)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
     return app
